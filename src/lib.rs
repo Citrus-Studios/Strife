14a0fdc25bb2d0b880b1035d36060cf26bb0018d
lonnonjamesd <lemonjameslem@protonmail.com>
2022-02-24 19:04:04 -0500

    Handle Resposne

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,49 @@
-use reqwest::header::{CONTENT_TYPE, CONTENT_LENGTH};
+use reqwest::Client;
+use reqwest::header::{CONTENT_TYPE, CONTENT_LENGTH, ACCEPT};
 use reqwest::header::{HeaderMap, USER_AGENT, HeaderValue, AUTHORIZATION};
 
-pub const DISCORD_API: &'static str = "https://discord.com/api";
-pub const USER_AGENT_VAL: &'static str = "DiscordBot (https://github.com/Citrus-Studios, 0.0.1)";
+pub const DISCORD_API: &'static str = "https://discord.com/api/v9/";
+pub const USER_AGENT_VAL: &'static str = "Strife (https://github.com/Citrus-Studios, 0.0.1)";
 
-#[test]
-fn test_api_test_fn() {
-    api_test();
+#[tokio::test]
+async fn test_api_test_fn() {
+    api_test().await;
 }
 
 // Early testing of the discord API
-fn api_test() {
+async fn api_test() {
 
-    let bot_token = "OTQ2NDc5Mjg3NzM1ODQwNzk4.YhfThg.2PHNRoLrczYKHYJzdaTK3g82WNs";
+    let bot_token = "Bot OTQ2NDc5Mjg3NzM1ODQwNzk4.YhfThg.2PHNRoLrczYKHYJzdaTK3g82WNs";
 
-    let mut headers = HeaderMap::with_capacity(3);
+    let client = Client::new();
+    let mut headers = HeaderMap::with_capacity(5);
     // Set the user agent header
     headers.insert(USER_AGENT, HeaderValue::from_static(USER_AGENT_VAL));
     // Set the authorization header
     headers.insert(AUTHORIZATION, HeaderValue::from_str(bot_token).unwrap());
     // Set the content type header
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-    // Content length
-    headers.insert(CONTENT_LENGTH, HeaderValue::from_str("0").unwrap());
+    // Accept Json
+    headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
+    // Length
+    headers.insert(CONTENT_LENGTH, HeaderValue::from_static("0"));
+
     
+    let response = client
+        .post(DISCORD_API)
+        .headers(headers)
+        .send()
+        .await;
+    
+    match response {
+        Ok(mut response) => {
+            // println!("{:?}", response);
+            println!("{:?}", response.status());
+            // println!("{:?}", response.headers());
+            println!("{:?}", response.text().await);
+        },
+        Err(e) => {
+            panic!("{:?}", e);
+        }
+    };
 }
