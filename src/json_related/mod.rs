e47496f4c628b2978b17e6860857e614a3f06d52
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-25 16:58:11 -0400

    User

--- src/json_related/mod.rs
+++ src/json_related/mod.rs
@@ -4,3 +4,4 @@ pub mod bot_gateway;
 pub mod properties;
 pub mod ready;
 pub mod session_start_limit;
+pub mod user;
---
+++ src/json_related/user.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize}; 
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct User {
+    id: i32,
+    username: String,
+    discriminator: String,
+    avatar: Option<String>,
+    bot: Option<bool>,
+    system: Option<bool>,
+    mfa_enabled: Option<bool>,
+    banner: Option<String>,
+    accent_color: Option<i32>,
+    locale: Option<String>,
+    verified: Option<i32>,
+    email: Option<String>,
+    flags: Option<i32>,
+    premium_type: Option<i32>,
+    public_flags: Option<i32>
+}
