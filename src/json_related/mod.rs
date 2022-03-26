d1d17b18eca079680fd162d75950e74ac01d8dd1
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-26 18:07:48 -0400

    reformatted + welcome_screen

--- src/json_related/mod.rs
+++ src/json_related/mod.rs
@@ -1,11 +1,12 @@
-pub mod ops;
-
 pub mod bot_gateway;
 pub mod properties;
 pub mod ready;
 pub mod session_start_limit;
 pub mod user;
-pub mod guild_related;
 pub mod presences;
 pub mod client_statuses;
+
+pub mod ops;
+pub mod guild_related;
 pub mod activities_related;
+pub mod welcomescreen_related;
