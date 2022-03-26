ad7fd6329475a58702f1310e0b9e7b544fc2606f
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-26 19:02:49 -0400

    Moved the types to a different crate for neatness

--- stife-types/src/lib.rs
+++ stife-types/src/lib.rs
@@ -11,3 +11,6 @@ pub mod ops;
 pub mod guild_related;
 pub mod activities_related;
 pub mod welcomescreen_related;
+
+pub type Snowflake = u64;
+pub type Timestamp = String;
--- stife-types/src/ready.rs
+++ stife-types/src/ready.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-use super::user::User; 
+use super::{user::User, guild_related::guild::Guild}; 
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Ready {
     v: i32,
     user: User,
-    guilds: ,
+    guilds: Vec<Guild>,
     session_id: String,
     shard: Option<Vec<i32>>,
-    application: ,
+    application: Vec<Application>,
 }
