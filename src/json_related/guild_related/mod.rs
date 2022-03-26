81fcb8254ffc86f5b98c9dd71a69cc59efde75ac
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-26 18:55:23 -0400

    Guild scheduled events

--- src/json_related/guild_related/mod.rs
+++ src/json_related/guild_related/mod.rs
@@ -1,14 +1,14 @@
 pub mod guild;
-pub mod channel_related;
 pub mod emojis;
 pub mod features;
 pub mod guild_members;
-pub mod guild_scheduled_events;
 pub mod presence_updates;
 pub mod roles;
 pub mod stage_instances;
 pub mod stickers;
 pub mod voice_states;
-pub mod welcome_screens;
 pub mod role_tags;
 pub mod overwrite;
+
+pub mod guild_scheduled_event_related;
+pub mod channel_related;
