3bf6d9c019c037fca801a268d0c9c4411448d240
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-26 18:31:18 -0400

    import

--- src/json_related/welcomescreen_related/welcomescreens.rs
+++ src/json_related/welcomescreen_related/welcomescreens.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use super::welcomescreen_channels::WelcomeScreenChannel;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct WelcomeScreen {
     description: Option<String>,
